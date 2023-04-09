7ec90f063c13788d92919b323a340af1e6b9cc0d
shattova <116936426+shattova@users.noreply.github.com>
2023-04-09 17:53:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,104 @@
 #include <map>
 #include "tstack.h"
 
+int getPrior(char op) {
+    switch (op) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+':
+        return 2;
+    case '-':
+        return 2;
+    case '*':
+        return 3;
+    case '/':
+        return 3;
+    }
+    return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    TStack<char, 100> stack1;
+    std::string str = "";
+    int result = 0;
+    for (char a : inf) {
+        bool truefalse = true;
+        if (getPrior(a) == 0) {
+            stack1.push(a);
+            truefalse = false;
+        }
+        if (getPrior(a) == -1) {
+            str = str + a;
+            str = str + ' ';
+            truefalse = false;
+        }
+        if (getPrior(a) > getPrior(stack1.get())) {
+            stack1.push(a);
+            truefalse = false;
+        }
+        if (stack1.isEmpty() && getPrior(a) != -1) {
+            stack1.push(a);
+            truefalse = false;
+        }
+        if (a != ')' && truefalse) {
+            while (getPrior(stack1.get()) >= getPrior(a)) {
+                str = str + stack1.pop();
+                str = str + ' ';
+            }
+            stack1.push(a);
+        }
+        if (a == ')') {
+            while (stack1.get() != '(') {
+                str = str + stack1.pop();
+                str = str + ' ';
+            }
+            stack1.pop();
+        }
+        if (result == inf.size() - 1) {
+            while (!stack1.isEmpty()) {
+                str = str + stack1.pop();
+                if (stack1.pri() != -1) {
+                    str = str + ' ';
+                }
+            }
+        }
+        ++result;
+    }
+    return str;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    TStack<int, 100> stack2;
+    for (char i : post) {
+        if (i == '+') {
+            int j = stack2.pop();
+            j = j + stack2.pop();
+            stack2.push(j);
+        }
+        if (i == '-') {
+            int j = stack2.pop();
+            j = stack2.pop() - j;
+            stack2.push(j);
+        }
+        if (i == '*') {
+            int j = stack2.pop();
+            j = j * stack2.pop();
+            stack2.push(j);
+        }
+        if (i == '/') {
+            int j = stack2.pop();
+            j = stack2.pop() / j;
+            stack2.push(j);
+        }
+        if (i == ' ') {
+            continue;
+        }
+        if ((i - '0') > 0) {
+            int j = i - '0';
+            stack2.push(j);
+        }
+    }
+    return stack2.get();
 }
